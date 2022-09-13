1e21633fd69407e85feb43635eb4fae99373b306
Richie Varghese <ric-v@outlook.com>
2022-09-13 23:21:32 +0530

    added font sources

--- app/root.tsx
+++ app/root.tsx
@@ -41,6 +41,9 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500;600;700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,500;1,600;1,900&family=Pacifico&family=Raleway:ital,wght@0,300;0,400;0,600;0,900;1,300;1,400;1,600;1,900&display=swap');
+        </style>
       </head>
       <body>
         <ThemeContext.Provider value={{ theme: colorMode, toggleTheme: toggleTheme }}>
