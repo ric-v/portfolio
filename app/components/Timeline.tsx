731e2f9e791f48edd5aea7800a8076541e100bd3
Richie Varghese <ric-v@outlook.com>
2022-09-27 13:09:43 +0530

    completed experience

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { transition } from '~/theme/animate'
+import { IoCloseSharp } from 'react-icons/io5'
 
 type Props = {
   duration: string
   title: string
   company: string
+  domain: string
   desc: string
+  children: React.ReactNode
 }
 
-const Timeline = ({ duration, title, company, desc }: Props) => {
+const Timeline = ({ duration, title, company, domain, desc, children }: Props) => {
+  const [selected, setSelected] = React.useState(false)
+
   return (
     <>
-      <div className={`col-span-3 lg:col-span-2 flex flex-row justify-center backdrop-blur-sm backdrop-brightness-95 rounded-2xl py-5 mx-2 neumorphic-shadow hover:backdrop-brightness-50 ${transition}`}>
+      <div className={`col-span-3 lg:col-span-2 flex flex-row justify-center backdrop-blur-sm backdrop-brightness-95 rounded-2xl py-5 mx-2 hover:backdrop-brightness-75  dark:neumorphic-shadow hover:scale-105 cursor-pointer ${transition}`} onClick={() => setSelected(true)}>
         <div className='text-center'>
           <div className='text-xs sm:text-md md:text-lg text-amber-800 dark:text-orange-300 animate-pulse'>
             {duration}
@@ -22,6 +27,9 @@ const Timeline = ({ duration, title, company, desc }: Props) => {
           <div className='text-md sm:text-xl md:text-2xl text-slate-800 dark:text-sky-300'>
             {company}
           </div>
+          <div className='text-sm sm:text-md md:text-lg text-slate-800 dark:text-sky-300'>
+            Domain: {domain}
+          </div>
           <div className='text-sm sm:text-md md:text-lg text-slate-600 dark:text-slate-300'>
             {desc.length > 30 ? `${desc.slice(0, 30)}...` : desc}
             <br />
@@ -29,7 +37,40 @@ const Timeline = ({ duration, title, company, desc }: Props) => {
           </div>
         </div>
       </div>
+
+      {/* if selected, show modal to display details */}
+      {selected && (
+        <div className='fixed top-0 left-0 w-full h-full flex flex-col justify-center items-center z-50 backdrop-blur-2xl backdrop-brightness-90'>
+          <div className='rounded-2xl p-2 md:p-10 mt-20 overflow-y-scroll scrollbar-thin'>
+            <div className='text-center'>
+              <div className='text-md md:text-xl text-amber-800 dark:text-orange-300 animate-pulse'>
+                {duration}
+              </div>
+              <div className='text-xl md:text-3xl mt-2 font-medium text-slate-800 dark:text-sky-300'>
+                {title}
+              </div>
+              <div className='text-xl md:text-3xl mt-2 text-slate-800 dark:text-sky-300'>
+                {company}
+              </div>
+              <div className='text-sm sm:text-md md:text-lg text-slate-800 dark:text-sky-300'>
+                Domain: {domain}
+              </div>
+              <div className='text-sm md:text-xl mt-2 text-slate-700 dark:text-slate-300'>
+                {desc}
+              </div>
+            </div>
+            <div className='text-center mt-2'>
+            {children}
+            </div>
+          </div>
+          <button className={`px-3 py-3 rounded-full hover:backdrop-brightness-50 hover:scale-110 hover:bg-black hover:bg-opacity-40 animate-pulse hover:animate-none ${transition}`} onClick={() => setSelected(false)}>
+            <IoCloseSharp size={32} />
+          </button>
+        </div>
+      )}
+
     </>
   )
 }
+
 export default Timeline;
--- app/routes/me.tsx
+++ app/routes/me.tsx
@@ -17,7 +17,7 @@ const Me = (props: Props) => {
   return (
     <AppLayout>
       <Parallax ref={parallax} pages={7} className="scrollbar-none">
-        {/* {Intro(parallax)} */}
+        {Intro(parallax)}
 
         {Experience(parallax)}
 
@@ -44,7 +44,7 @@ const Me = (props: Props) => {
           onClick={() => parallax.current.scrollTo(0)}
         >
           <div className="grid grid-cols-3 md:grid-cols-6 gap-4 mt-5">
-            <div className="bg-black rounded-xl p-2">01</div>
+            <div className="bg-black rounded-xl p-2" on>01</div>
             <div className="bg-black rounded-xl p-2">02</div>
             <div className="bg-black rounded-xl p-2">03</div>
             <div className="col-span-2 rounded-xl bg-black p-2">04</div>
@@ -82,7 +82,7 @@ const Intro = (parallax: React.MutableRefObject<IParallax>) => {
           <div className="col-span-3 h-20 md:col-span-2" />
           <div className="col-span-3 md:col-span-2 bg-amber-400 dark:bg-sky-500 bg-opacity-10 dark:bg-opacity-10 h-64 rounded-3xl backdrop-blur-sm shadow-2xl -rotate-6 m-5">
             <div className="h-full w-full flex flex-col justify-center items-center">
-              <div className="text-2xl font-medium text-center text-slate-700 dark:text-sky-300">
+              <div className="text-xl md:text-3xl font-medium text-center text-slate-700 dark:text-sky-300">
                 Hi, this is <span className="font-bold">Richie</span> 👋, a <span className={`font-bold`}>Full Stack Engineer</span> from Bengaluru, India.
               </div>
             </div>
@@ -98,21 +98,21 @@ const Intro = (parallax: React.MutableRefObject<IParallax>) => {
         <div className="grid grid-cols-3 md:grid-cols-5 gap-4 text-lg font-bold">
           <div className="col-span-3 md:col-span-1" />
           <a
-            className={`col-span-3 md:col-span-1 flex flex-row justify-center py-2 backdrop-blur-sm rounded-2xl hover:shadow-2xl text-slate-600 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-200 transition-all duration-1000`}
+            className={`col-span-3 md:col-span-1 flex flex-row justify-center py-2 backdrop-blur-sm rounded-2xl hover:shadow-2xl text-slate-600 hover:text-slate-800 dark:text-slate-400 dark:hover:text-slate-200 hover:scale-110 transition-all duration-1000`}
             href="https://github.com/ric-v" target={'_blank'} rel={'noreferrer'}
           >
             <span className="mr-2">Github</span>
             <VscGithubInverted size={32} />
           </a>
           <a
-            className={`col-span-3 md:col-span-1 flex flex-row justify-center py-2 backdrop-blur-sm rounded-2xl hover:shadow-2xl text-slate-600 hover:text-sky-700 dark:text-slate-400 dark:hover:text-sky-500 transition-all duration-1000`}
+            className={`col-span-3 md:col-span-1 flex flex-row justify-center py-2 backdrop-blur-sm rounded-2xl hover:shadow-2xl text-slate-600 hover:text-sky-700 dark:text-slate-400 dark:hover:text-sky-500 hover:scale-110 transition-all duration-1000`}
             href="https://www.linkedin.com/in/ric-v/" target={'_blank'} rel={'noreferrer'}
           >
             <span className="mr-2">LinkedIn</span>
             <GrLinkedinOption size={32} />
           </a>
           <a
-            className={`col-span-3 md:col-span-1 flex flex-row justify-center py-2 backdrop-blur-sm rounded-2xl hover:shadow-2xl text-slate-600 hover:text-red-700 dark:text-slate-400 dark:hover:text-red-400 transition-all duration-1000`}
+            className={`col-span-3 md:col-span-1 flex flex-row justify-center py-2 backdrop-blur-sm rounded-2xl hover:shadow-2xl text-slate-600 hover:text-red-700 dark:text-slate-400 dark:hover:text-red-400 hover:scale-110 transition-all duration-1000`}
             href="https://drive.google.com/file/d/1-34NxUJF_Fj6-s4vUZVZIjIVO0VD-WX9/view?usp=sharing" target={'_blank'} rel={'noreferrer'}
           >
             <span className="mr-2">Resume</span>
@@ -128,20 +128,61 @@ const Experience = (parallax: React.MutableRefObject<IParallax>) => {
   return (
     <>
       <ParallaxLayer
-        offset={0}
+        offset={1}
         speed={0}
         className="bg-sky-200 dark:bg-sky-700 bg-opacity-20 dark:bg-opacity-20 h-screen flex flex-col justify-center px-2"
         onClick={() => parallax.current.scrollTo(2)}
       />
       <ParallaxLayer
-        offset={-0.1}
+        offset={0.9}
         speed={0}
         className="h-screen flex flex-col justify-center mt-28 px-2"
       >
-        <div className="grid grid-cols-3 lg:grid-cols-6 gap-4">
-          <Timeline duration="Jun 2018 - May 2021" title="Software Developer L2" company="Algomox Pvt. Ltd." desc="An employee since start of the company. Worked on 2 major platform building cycle and most of the back-end server development. Associated with chief engineer to architect and design solutions based on the platforms. Had the opportunity to build servers from scratch, design them, document and debug the applications." key='1' />
-          <Timeline duration="Jun 2021 - Sept 2022" title='Engineer - Software Development' company="Schnell Digital Technologies" desc='Worked as part of the initial team for building PBM engine for processing real-time pharmacy claims request for clients at high scale and high volume. Primarily worked on the processing engine with concurrent processing and adjudication capabilities based on business rules.' key='2' />
-          <Timeline duration="Oct 2022 - Preset" title="Senior Engineer - Software Development" company="Schnell Digital Technologies" desc='Worked on the PBM claim processing engine for high scale and high volume pharmacy requests from clients. Worked on integration with external switch providers for managing the entire lifecycle of claims and their processing.' />
+        <p className={`text-2xl md:text-4xl font-bold text-center text-slate-700 dark:text-sky-300 mb-5 hover:scale-110 cursor-none ${transition}`}>
+          Work Experience
+        </p>
+        <div className="grid grid-cols-3 lg:grid-cols-6 gap-2">
+          <Timeline duration="Jun 2018 - May 2021" title="Software Developer L2" company="Algomox Pvt. Ltd." domain="AIOPS & MLOPS" desc="An employee since start of the company. Worked on 2 major platform building cycle and most of the back-end server development. Associated with chief engineer to architect and design solutions based on the platforms. Had the opportunity to build servers from scratch, design them, document and debug the applications." key='1'>
+            <>
+              <p className="text-xl md:text-3xl mt-2 text-slate-800 dark:text-sky-300">Major roles played and contributions</p>
+              <ul className="text-center text-sm md:text-lg text-slate-700 dark:text-slate-300 mt-2">
+                <li>Senior back-end developer, system administrator and SCM</li>
+                <li>Assisted in solution design and architecture</li>
+                <li>Microservice based server development in GO and Python</li>
+                <li>REST API based integration with in-house AI solutions and external tools</li>
+                <li>Automation and system operations with headless server development in GO and shell scripts</li>
+                <li>Byte-code engineering experience in Java (ASM) for high volume and dynamic data manipulation</li>
+                <li>Technical designing and functional documentation of developed products</li>
+                <li>Technical demo with customers and experience in deploying products at customer’s end</li>
+              </ul>
+            </>
+          </Timeline>
+
+          <Timeline duration="Jun 2021 - Sept 2022" title='Engineer - Software Development' company="Schnell Digital Technologies" domain="PBM (Pharmacy Benefit Management)" desc='Worked as part of the initial team for building PBM engine for processing real-time pharmacy claims request for clients at high scale and high volume. Primarily worked on the processing engine with concurrent processing and adjudication capabilities based on business rules.' key='2' >
+            <>
+              <p className="text-xl md:text-3xl mt-2 text-slate-800 dark:text-sky-300">Major roles played and contributions</p>
+              <ul className="text-center text-sm md:text-lg text-slate-700 dark:text-slate-300 mt-2">
+                <li>Golang developer working on socket connection based micro-services for claim processing</li>
+                <li>Worked with the team to integrate SQL functions and logic with processing engine</li>
+                <li>Did POC with team to add server logging and tracing with Prometheus</li>
+              </ul>
+            </>
+          </Timeline>
+
+          <Timeline duration="Oct 2022 - Preset" title="Senior Engineer - Software Development" company="Schnell Digital Technologies" domain="PBM" desc='Worked on the PBM claim processing engine for high scale and high volume pharmacy requests from clients. Worked on integration with external switch providers for managing the entire lifecycle of claims and their processing.' >
+            <>
+              <p className="text-xl md:text-3xl mt-2 text-slate-800 dark:text-sky-300">Major roles played and contributions</p>
+              <ul className="text-center text-sm md:text-lg text-slate-700 dark:text-slate-300 mt-2">
+                <li>Senior Golang developer working on multiple REST API based services for product/external switch integration and on concurrent processing engine in micro-service architecture based on socket programming</li>
+                <li>Built unit test for each functionalities in all services for better code coverage and initial testing for devs</li>
+                <li>Built internal Golang modules/packages on azure git to be shared between multiple services</li>
+                <li>Azure pipelines and release management</li>
+                <li>Product dockerization and its deployment implemented with team members</li>
+                <li>Postgres PL/pgSQL based stored procedures built for efficient and modularized queries for processing claims / statistics</li>
+                <li>Microsoft Azure cloud skills developed with the help of team members for managing cloud infra</li>
+              </ul>
+            </>
+          </Timeline>
         </div>
       </ParallaxLayer>
     </>
@@ -152,13 +193,13 @@ const Projects = (parallax: React.MutableRefObject<IParallax>) => {
   return (
     <>
       <ParallaxLayer
-        offset={2}
+        offset={0}
         speed={0}
         className="bg-sky-500 dark:bg-sky-900 bg-opacity-20 dark:bg-opacity-30 h-screen flex flex-col justify-center px-2"
         onClick={() => parallax.current.scrollTo(3)}
       />
       <ParallaxLayer
-        offset={1.9}
+        offset={0}
         speed={0}
         className="h-screen flex flex-col justify-center px-2"
         onClick={() => parallax.current.scrollTo(3)}
@@ -166,24 +207,6 @@ const Projects = (parallax: React.MutableRefObject<IParallax>) => {
         <div className="grid grid-cols-3 md:grid-cols-6 gap-4">
           <div className="col-span-3 h-20 md:col-span-2" />
 
-        </div>
-      </ParallaxLayer>
-      <ParallaxLayer
-        offset={1.9}
-        speed={0}
-        className="h-screen flex flex-col justify-end px-2"
-      >
-        <div className="grid grid-cols-12 md:grid-cols-12 gap-4">
-          <div className="col-span-10 md:col-span-11" />
-          <div className="col-span-2 md:col-span-1 cursor-pointer" onClick={() => parallax.current.scrollTo(1)}>
-            <IoIosArrowUp className="text-4xl text-slate-700 dark:text-sky-300 animate-bounce" />
-          </div>
-        </div>
-        <div className="grid grid-cols-12 md:grid-cols-12 gap-4">
-          <div className="col-span-10 md:col-span-11" />
-          <div className="col-span-2 md:col-span-1 cursor-pointer" onClick={() => parallax.current.scrollTo(3)}>
-            <IoIosArrowDown className="text-4xl text-slate-700 dark:text-sky-300 animate-bounce" />
-          </div>
         </div>
       </ParallaxLayer>
     </>
