a2196e9036a2db3accc7e585ff1977f875bc01e5
ric-v <ric-v@outlook.com>
2023-01-26 12:14:50 +0530

    removed remix components

--- app/github-api/auth.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-export const ghContext = createContext({
-  data: [] as GithubRepoType[],
-  setData: (data: GithubRepoType[]) => {},
-});
--- app/theme/context.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-export const ThemeContext = createContext({
-  theme: "dark" as "dark" | "light",
-  toggleTheme: () => {},
-});
